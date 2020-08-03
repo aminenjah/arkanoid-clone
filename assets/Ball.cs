84e17b37a0168051b67ab74f4ed2f7dd8c18df1f
aminenjah <njahamine@yahoo.com>
2020-08-03 11:06:39 +0200

    first commit

---
+++ assets/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject panel;
+    public float speed = 100.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+    }
+    void gameover()
+    {
+        panel.SetActive(true);
+        Time.timeScale = 0;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "block")
+        {
+            Destroy(collision.gameObject);
+        }
+        if(collision.gameObject.name == "background")
+        {
+            gameover();
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ assets/racket.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class racket : MonoBehaviour
+{
+    // movement speed 
+    public float speed = 150;
+   
+    float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth)
+    {
+        return (ballPos.x - racketPos.x) / racketWidth;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if(collision.gameObject.name == "racket")
+        {
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 dir = new Vector2(x, 1).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed; 
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        // Get  Horizantal Input 
+        float h = Input.GetAxisRaw("Horizontal");
+        // set velocity ( movemnt direction * speed )
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
+    }
+}
